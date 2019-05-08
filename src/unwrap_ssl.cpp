aaf4ed560b98c13563bcd6eccb311da74c73b9e3
Alba Mendez <alba.mendez@basetis.com>
2019-05-08 18:01:04 +0200

    Fix

--- src/unwrap_ssl.cpp
+++ src/unwrap_ssl.cpp
@@ -2,9 +2,9 @@
 
 #define NODE_WANT_INTERNALS 1
 #include <tls_wrap.h>
-#include <js_native_api_v8.h>
 
 using v8::Local;
+using v8::Value;
 using v8::String;
 using v8::Object;
 using v8::Isolate;
@@ -13,9 +13,15 @@ class TLSWrap2 : public node::TLSWrap {
     public: SSL* get_ssl(){ return ssl_.get(); }
 };
 
-std::string v8_local_obj_ctor(Local<Object> obj) {
+static std::string v8_local_obj_ctor(Local<Object> obj) {
     Local<String> ctor = obj->GetConstructorName();
     return *String::Utf8Value(Isolate::GetCurrent(), ctor);
+}
+
+static Local<Object> v8_local_obj_from_napi_value(napi_value v) {
+  Local<Value> local;
+  memcpy(&local, &v, sizeof(v));
+  return local.As<Object>();
 }
 
 SSL* unwrap_ssl(napi_env env, Napi::Object socket) {
@@ -23,7 +29,7 @@ SSL* unwrap_ssl(napi_env env, Napi::Object socket) {
     if (!wrap_js.IsObject())
         throw Napi::TypeError::New(env, "'_handle' property is not an object");
 
-    Local<Object> wrap_v8 = v8impl::V8LocalValueFromJsValue(wrap_js).As<Object>();
+    Local<Object> wrap_v8 = v8_local_obj_from_napi_value(wrap_js);
     if (v8_local_obj_ctor(wrap_v8)!="TLSWrap")
         throw Napi::TypeError::New(env, "'_handle' property is not TLSWrap");
     return TLSWrap2::FromJSObject<TLSWrap2>(wrap_v8)->get_ssl();
