3b520280f5ec29ae35703d5b96c4d376f6a64b00
Alba Mendez <alba.mendez@basetis.com>
2019-05-08 17:33:35 +0200

    Refactor main

--- src/main.cpp
+++ src/main.cpp
@@ -6,32 +6,26 @@
 #error OpenSSL 1.1.0+ required
 #endif
 
-napi_value get_client_random(napi_env env, SSL *ssl) {
-    auto buf = Napi::Buffer<unsigned char>::New(env, SSL3_RANDOM_SIZE);
-    SSL_get_client_random(ssl, buf.Data(), SSL3_RANDOM_SIZE);
-    return buf;
-};
-
-napi_value get_master_key(napi_env env, SSL *ssl) {
-    auto buf = Napi::Buffer<unsigned char>::New(env, SSL_MAX_MASTER_KEY_LENGTH);
-    SSL_SESSION *session = SSL_get_session(ssl);
-    if (!session)
-        throw Napi::Error::New(env, "No TLS session present");
-    SSL_SESSION_get_master_key(session, buf.Data(), SSL_MAX_MASTER_KEY_LENGTH);
-    return buf;
-};
-
 Napi::Object get_session_key(const Napi::CallbackInfo& info) {
     Napi::Env env = info.Env();
 
     if (info.Length()<1 || !info[0].IsObject())
-        throw Napi::TypeError::New(env, "get_session_key() expects object");
+        throw Napi::TypeError::New(env, "get_session_key() expects TLSSocket argument");
 
     SSL* ssl = unwrap_ssl(env, info[0].As<Napi::Object>());
+    SSL_SESSION* session = SSL_get_session(ssl);
+    if (!session)
+        throw Napi::Error::New(env, "No TLS session present");
 
-    Napi::Object result = Napi::Object::New(env);   
-    result.Set("client_random", get_client_random(env, ssl));
-    result.Set("master_key", get_master_key(env, ssl));
+    auto buf1 = Napi::Buffer<unsigned char>::New(env, SSL3_RANDOM_SIZE);
+    SSL_get_client_random(ssl, buf1.Data(), SSL3_RANDOM_SIZE);
+
+    auto buf2 = Napi::Buffer<unsigned char>::New(env, SSL_MAX_MASTER_KEY_LENGTH);
+    SSL_SESSION_get_master_key(session, buf2.Data(), SSL_MAX_MASTER_KEY_LENGTH);
+
+    Napi::Object result = Napi::Object::New(env);
+    result.Set("client_random", buf1);
+    result.Set("master_key", buf2);
     return result;
 }
 
@@ -40,4 +34,4 @@ Napi::Object init(Napi::Env env, Napi::Object exports) {
     return exports;
 }
 
-NODE_API_MODULE(sslkeylog, init)
+NODE_API_MODULE(NODE_GYP_MODULE_NAME, init)
